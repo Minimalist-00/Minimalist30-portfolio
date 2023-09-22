5e454292603f010b35861e235c001af2a83da3c6
Minimalist-00 <kouki4122@icloud.com>
2023-09-22 22:59:43 +0900

    トップページ少しだけ

---
+++ src/components/Layout.tsx
@@ -0,0 +1,29 @@
+import { Box } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+type LayoutProps = {
+  children: ReactNode
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <Box maxW="1100px" mx="auto" bg="gray.100" mt="50px">
+      <Header />
+      <Box
+        as="main"
+        maxW="900px" // メインコンテンツの最大横幅を設定
+        mx="auto" // 横方向の中央寄せを行う
+        mt="20px" // 上部の余白を設定
+        p="20px" // コンテンツ内の余白を設定
+        bg="pink" // 背景色を設定
+      >
+        <Box>{children}</Box>
+      </Box>
+      <Footer />
+    </Box>
+  )
+}
+
+export default Layout
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,20 +1,19 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import { Box } from '@chakra-ui/react'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
-    <>
+    <Box as="main">
       <Head>
         <title>Minimalist30</title>
         <meta name="description" content="説明が入ります" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>トップページです</main>
-    </>
+      <Box>トップページです</Box>
+    </Box>
   )
 }
