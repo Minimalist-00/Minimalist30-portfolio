8ac8de86bda39084e81efa83f8092c1de898eb68
Minimalist-00 <kouki4122@icloud.com>
2023-10-06 22:27:12 +0900

    :art: Portfolioページ、側だけ完成

--- src/components/Portfolio/PortfolioList.tsx
+++ src/components/Portfolio/PortfolioList.tsx
@@ -1,6 +1,90 @@
 const PortfolioList = () => {
+
+  const cardsData = [
+    {
+      imageUrl: 'testImage/karaoke.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+    {
+      imageUrl: 'testImage/game.jpg',
+      title: 'カードタイトル',
+      tags: ['Typescript', 'Next.js', 'Tailwind CSS', 'Figma', 'Vercel'],
+    },
+  ];
+
+  interface CardProps {
+    imageUrl: string;
+    title: string;
+    tags: string[];
+  }
+
+  const Card: React.FC<CardProps> = ({ imageUrl, title, tags }) => {
+    return (
+      <div className="max-w-sm bg-white shadow-lg rounded-xl overflow-hidden mx-1 mb-8">
+        <img src={imageUrl} alt={title} className="w-full h-44 object-cover" />
+        <div className="px-3 py-3">
+          <div className="font-bold text-lg mb-2">{title}</div>
+          <div className="flex flex-wrap gap-2">
+            {tags.map((tag, index) => (
+              <span
+                key={index}
+                className={`text-white font-semibold text-[11px] py-1 px-2 rounded-2xl bg-[#7a797a]`}
+              >{tag}</span>
+            ))}
+          </div>
+        </div>
+      </div>
+    );
+  };
+
   return (
-    <div>PortfolioList</div>
+    <div>
+      <div className="lg:max-w-main mx-auto pt-14 pb-14">
+        <section>
+          <div className="grid grid-cols-3 gap-4">
+            {cardsData.map((card, index) => (
+              <Card
+                key={index}
+                imageUrl={card.imageUrl}
+                title={card.title}
+                tags={card.tags}
+              />
+            ))}
+          </div>
+        </section>
+      </div>
+    </div>
   )
 }
 
--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,11 +1,12 @@
+import PortfolioList from "@/components/Portfolio/PortfolioList"
 import PortfolioOverview from "@/components/Portfolio/PortfolioOverview"
 
 const portfolio = () => {
   return (
-    <div>
+    <>
       <PortfolioOverview />
-
-    </div>
+      <PortfolioList />
+    </>
   )
 }
 
